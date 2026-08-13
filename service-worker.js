01d1c7b900a95634f1c7e4c1eea1ea28139ebe96
maasaa070 <maasaa070@gmail.com>
2026-08-13 22:52:48 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,48 @@
-const CACHE_NAME = "essence-of-ins-rana-v1";
+const CACHE_NAME = "essence-of-ins-rana-v2";
+
+const ABOUT_ASSETS = [
+  "/about.html",
+  "/style.css?v=20260617"
+];
 
 self.addEventListener("install", (event) => {
-  console.log("Service Worker: Installed");
-  self.skipWaiting();
+  console.log("Service Worker: Installing");
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ABOUT_ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Service Worker: Activated");
-  event.waitUntil(clients.claim());
+  console.log("Service Worker: Activating");
+
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames
+            .filter((cacheName) => cacheName !== CACHE_NAME)
+            .map((cacheName) => caches.delete(cacheName))
+        );
+      })
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  // Network only for now.
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(event.request);
+      })
+  );
+
 });
